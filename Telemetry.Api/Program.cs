4957e11b83249f85da3763c7e3f02c7da07dafd4
alexmonteil <alex.monteil@outlook.com>
2026-05-22 00:42:30 -0500

    Added JWT Auth packages and configuration

--- Telemetry.Api/Program.cs
+++ Telemetry.Api/Program.cs
@@ -1,6 +1,42 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+// REGISTRATION OF SERVICES WITH DI CONTAINER
+
+// Read environment variables for JWT auth
+var secretKey = Environment.GetEnvironmentVariable("JWT_SECRET_KEY")
+    ?? throw new InvalidOperationException("Critical Failure: JWT_SECRET_KEY environment variable is not set.");
+var issuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? "TelemetryDefaultIssuer";
+var audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE") ?? "TelemetryDefaultAudience";
+
+
+// Register JWT Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+        ClockSkew = TimeSpan.Zero 
+    };
+});
 
 // Read variables directly from the environment (loaded from your .env file)
 var dbHost = "localhost";
@@ -10,8 +46,6 @@ var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "devuser";
 var dbPass = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "devpassword";
 
 var connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPass}";
-
-// Add services to the container.
 
 // Register PostgreSQL
 builder.Services.AddDbContext<TelemetryDbContext>(options => options.UseNpgsql(connectionString));
@@ -28,6 +62,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
